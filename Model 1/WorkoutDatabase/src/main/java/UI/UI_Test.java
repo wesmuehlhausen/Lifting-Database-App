1e1463cedb4a45282d2b080782a0603c5b72cb00
Wesley Muehlhausen <wesmuehlhausen@gmail.com>
2022-12-05 19:25:46 -0800

    First UI

---
+++ Model 1/WorkoutDatabase/src/main/java/UI/UI_Test.java
@@ -0,0 +1,11 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UI_Test {
+    public static void main(String[] args) {
+        TestForm testForm = new TestForm();
+    }
+}
+
